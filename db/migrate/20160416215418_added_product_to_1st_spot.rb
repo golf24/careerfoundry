d203a0131ca3f42d467dd85c830f1061acb21b9a
Lawson <lawsongongdds@gmail.com>
2016-04-16 16:55:09 -0500

    Migrated Database

---
+++ db/migrate/20160416215418_added_product_to_1st_spot.rb
@@ -0,0 +1,4 @@
+class AddedProductTo1stSpot < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415195821) do
+ActiveRecord::Schema.define(version: 20160416215418) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
